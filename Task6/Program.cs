4ede28a9a0353561fed56b88961ec4721e6ed38f
Alexander <ggttbevg1990@gmail.com>
2023-05-24 23:52:01 +0300

    Сделал 4 и 6 задания

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число:");
+int i = Convert.ToInt32(Console.ReadLine());
+
+    if (i % 2 == 1)
+        {
+        Console.WriteLine("Число " + i + " является: Нечетным");
+        }
+    else
+        {
+        Console.WriteLine("Число " + i + " является: Четным");
+        }
