7c65d4374baf1c82eceeeda8e109d9e1469942a1
Alexander <ggttbevg1990@gmail.com>
2023-05-25 00:06:19 +0300

    Сделал 8 задание

---
+++ Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int i = 1;
+bool not = true;
+
+Console.Write("Введите число:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + m);
+    while (i <= m)
+    {
+        if (i % 2 != 1)
+            {
+            Console.Write(i + ", ");
+            not = false;
+            }
+            i++;
+    }
+
+        if (not)
+        {
+            Console.WriteLine("Нет чётных чисел!");
+        }
