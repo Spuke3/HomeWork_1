1d957d66696973ca09808b806ad8c1ca74d044bc
Alexander <ggttbevg1990@gmail.com>
2023-05-24 19:58:44 +0300

    Сделал второе задание

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 3-ие число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (i > n)
+ if (i > m)
+ Console.Write("Максимальное число: " + i);
+ else
+ Console.Write("Максимальное число: " + m);
+else if (n > m)
+ Console.Write("Максимальное число: " + n);
+else
+ Console.Write("Максимальное число: " + m);
+
